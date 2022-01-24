436d153094ebf03bb6a695ebcf4024a666b2111c
Fransiskus <aldzaidea@gmail.com>
2022-01-24 20:52:44 +0000

    add shortend req.param

--- express_server.js
+++ express_server.js
@@ -1,15 +1,19 @@
+//requiring all module needed
 const { Template } = require("ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//set engine so we can read ejs file from views folder and render it as html file
 app.set("view engine", "ejs");
 
+//temporary database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+//get request from server for several path
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -28,6 +32,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortUrl", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
+
+//listening port 8080 and console log the port everytime we connect to the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
