1dabd54c6b220b270ed54495e3abae7a752d5d77
Fransiskus <aldzaidea@gmail.com>
2022-01-24 23:16:47 +0000

    create function to generate random alphanumeric shortURL

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 //requiring all module needed
 const { Template } = require("ejs");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+
+//user body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //set engine so we can read ejs file from views folder and render it as html file
 app.set("view engine", "ejs");
@@ -12,6 +16,23 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+//generate 6 random alpha numeric function
+const generateRandomString = () => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let alphanumeric = "";
+  for (let i = 0; i < 6; i++) {
+    alphanumeric += possible.charAt(Math.random() * possible.length);
+  }
+  return alphanumeric;
+};
+
+app.post("/urls", (req, res) => {
+  const shortRandomUrl = generateRandomString();
+  urlDatabase[shortRandomUrl] = req.body.longURL;
+  res.render("urls_index");
+});
 
 //get request from server for several path
 app.get("/", (req, res) => {
@@ -32,16 +53,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  const templateVars = {};
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
-});
-
-app.get("/example/:apple/:pear", (req, res) => {
-  console.log(req.params);
 });
 
 //listening port 8080 and console log the port everytime we connect to the server
